bf1b750f8c181eac685bfc6a102f37bd7cd72f93
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-04-18 12:25:13 +0300

    Reproduce the error

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,64 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
+
+import Control.Monad.Logger (MonadLogger (..), fromLogStr, toLogStr)
+import Control.Monad.Trans.Reader
+import Data.Pool
+import qualified Data.String.Class as S
+import qualified Data.Text as T
+import Database.Persist.Postgresql
+import qualified Database.Persist.Postgresql as P
+import Database.Persist.TH
+import UnliftIO
+
+-- | 'P.withPostgresqlPool' needs this.
+instance MonadLogger IO where
+  monadLoggerLog _loc _logSource _logLevel msg =
+    liftIO (S.hPutStrLn stderr (fromLogStr (toLogStr msg)))
+
+share
+  [mkPersist sqlSettings, mkDeleteCascade sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+-- Maps entity to its proper form
+NamedPropers
+    entity T.Text
+    proper T.Text
+    Primary entity
+|]
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  let connString = "host=localhost port=5432 user=postgres dbname=persistent_transaction_weirdness password=password"
+  P.withPostgresqlPool (S.fromText connString) 5 $ \pool ->
+    flip P.runSqlPool pool $ P.runMigration migrateAll
+  let streamOfNames = take 1000000 (cycle ["John", "Paul", "George", "Ringo"])
+  P.withPostgresqlPool (S.fromText connString) 5 $ \pool ->
+    UnliftIO.pooledForConcurrentlyN_ 8 streamOfNames $ \name -> do
+      runDb pool $ do
+        mNEProp <- P.get (NamedPropersKey name)
+        case mNEProp of
+          Just neProp -> do
+            pure ()
+          Nothing -> do
+            P.repsert
+              (NamedPropersKey name)
+              ( NamedPropers
+                  { namedPropersEntity = name,
+                    namedPropersProper = T.toLower name
+                  }
+              )
+
+runDb :: Pool P.SqlBackend -> ReaderT P.SqlBackend IO b -> IO b
+runDb pool f = do
+  liftIO $ flip P.runSqlPool pool $ f
